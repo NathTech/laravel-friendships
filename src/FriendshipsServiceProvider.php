b07c4d8b6ae6b86ffc269c1a58edd0a4c6e5f3c8
Nathaniel Fenton <nathinabob@gmail.com>
2020-08-07 17:10:28 +0100

    conditionally load or publish migrations

--- src/FriendshipsServiceProvider.php
+++ src/FriendshipsServiceProvider.php
@@ -16,11 +16,14 @@ class FriendshipsServiceProvider extends ServiceProvider
         if (class_exists('CreateFriendshipsTable') || class_exists('CreateFriendshipsGroupsTable')) {
             return;
         }
-        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
-        $this->publishes([
-            __DIR__ . '/../migrations/0000_00_00_000000_create_friendships_table.php'        => database_path('migrations') . '/' . date('Y_m_d_His', time()) . '_create_friendships_table.php',
-            __DIR__ . '/../migrations/1000_00_00_000000_create_friendships_groups_table.php' => database_path('migrations') . '/' . date('Y_m_d_His', time() + 1) . '_create_friendships_groups_table.php'
-        ], 'migrations');
+        if (method_exists($this, 'loadMigrationsFrom')) {
+            $this->loadMigrationsFrom(__DIR__ . '/../migrations');
+        } else {
+            $this->publishes([
+                __DIR__ . '/../migrations/0000_00_00_000000_create_friendships_table.php'        => database_path('migrations') . '/' . date('Y_m_d_His', time()) . '_create_friendships_table.php',
+                __DIR__ . '/../migrations/1000_00_00_000000_create_friendships_groups_table.php' => database_path('migrations') . '/' . date('Y_m_d_His', time() + 1) . '_create_friendships_groups_table.php'
+            ], 'migrations');
+        }
         $this->publishes([__DIR__ . '/../config/friendships.php' => config_path('friendships.php')], 'config');
     }
 
